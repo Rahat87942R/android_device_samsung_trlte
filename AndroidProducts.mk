ce83a27728cf74aef71f45e92c64e0bf97e39ac7
Zero_0R <80145720+Rahat87942R@users.noreply.github.com>
2022-03-21 20:48:17 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,9 +16,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/lineage_j7elte.mk
+    $(LOCAL_PATH)/havoc_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_j7elte-user \
-    lineage_j7elte-userdebug \
-    lineage_j7elte-eng
+    havoc_j7elte-user \
+    havoc_j7elte-userdebug \
+    havoc_j7elte-eng
